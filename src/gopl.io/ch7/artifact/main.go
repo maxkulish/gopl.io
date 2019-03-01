0a5c88fbfd952e4d78dc1f4c01f07ce402385d2e
Max Kulish <kma.memo@gmail.com>
2019-03-01 09:26:31 +0200

    artifact

---
+++ src/gopl.io/ch7/artifact/main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"io"
+	"time"
+)
+
+type Artifact interface {
+	Title() string
+	Creators() []string
+	Created() time.Time
+}
+
+type Text interface {
+	Pages() int
+	Words() int
+	PageSize() int
+}
+
+type Audio interface {
+	Stream() (io.ReadCloser, error)
+	RunningTime() time.Duration
+	Format() string // e.g., "MP3", "WAV"
+}
+
+type Video interface {
+	Stream() (io.ReadCloser, error)
+	RunningTime() time.Duration
+	Format() string // e.g, "MP4", "WMV"
+	Resolution() (x, y int)
+}
+
+type Streamer interface {
+	Stream() (io.ReadCloser, error)
+	RunningTime() time.Duration
+	Format() string
+}
+
+func main() {
+
+}
