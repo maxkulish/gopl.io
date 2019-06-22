dbf7f60ddf64a9c6e2bd717107538ffa414b1008
Max Kulish <kma.memo@gmail.com>
2019-06-22 11:46:48 +0300

    Testing

---
+++ src/gopl.io/ch11/storage/storage.go
@@ -0,0 +1,43 @@
+// Storage is a quota-checking service.
+// Whet users exceed 90% of their quota, the system sends them an email
+package storage
+
+import (
+	"fmt"
+	"log"
+	"net/smtp"
+)
+
+var usage = make(map[string]int64)
+
+func bytesInUse(username string) int64 { return usage[username] }
+
+// Email sender configuration
+const (
+	sender   = "notificatoin@gmail.com"
+	password = "somepass"
+	hostname = "smtp.example.com"
+)
+
+const template = `Warning: you are using %d bytes of storage,
+%d%% of your quota`
+
+var notifyUser = func(username, msg string) {
+	auth := smtp.PlainAuth("", sender, password, hostname)
+	err := smtp.SendMail(hostname+":587", auth, sender,
+		[]string{username}, []byte(msg))
+	if err != nil {
+		log.Printf("smtp.Sendmail(%s) failed: %s", username, err)
+	}
+}
+
+func CheckQuota(username string) {
+	used := bytesInUse(username)
+	const quota = 1000000000 // 1GB
+	percent := 100 * used / quota
+	if percent < 90 {
+		return // OK
+	}
+	msg := fmt.Sprintf(template, used, percent)
+	notifyUser(username, msg)
+}
